f950fbdcf12670077ae2b27a925f84e1848e642f
JARS3N <JARS3N@users.noreply.github.com>
2023-01-20 11:02:56 -0500

    Update test_specs.R

--- R/test_specs.R
+++ R/test_specs.R
@@ -1,6 +1,6 @@
 test_specs <- function(targets,ctg_means){
 # takes self$targets & self$ctg_means
-vals<-as.numeric(setNames(ctg_means$Values,ctg_means$attributes))
+vals<-setNames(as.numeric(ctg_means$Values),ctg_means$attributes)
 OUT<- NULL
 OUT[1] <- vals['pH.LED.avg'] >= targets$LED_LOW &  vals['pH.LED.avg'] <=  targets$pH_LED_high
 OUT[2] <- vals['pH.LED.CV'] < 30
@@ -10,6 +10,6 @@ OUT[5] <- vals['O2.LED.Avg'] >= targets$LED_LOW &  vals['O2.LED.Avg'] <=  target
 OUT[6] <- vals['O2.LED.CV'] < 30
 OUT[7] <- vals['KSV.Avg'] >= (.9 * targets$ksv) & vals['KSV.Avg'] <= (1.1 * targets$ksv)
 OUT[8] <- vals['KSV.CV'] < 5
-c("FAIL","PASS")[factor(OUT)]
+c("FAIL","PASS")[factor(OUT,levels=c(F,T)]
 
 }
