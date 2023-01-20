197b64f35307a69d8b5e9603055a9193bcce0199
JARS3N <JARS3N@users.noreply.github.com>
2023-01-20 10:32:40 -0500

    Update sn_means.R

--- R/sn_means.R
+++ R/sn_means.R
@@ -1,9 +1,10 @@
 sn_means <- function(wetqc) {
   group_by(wetqc, sn) %>%
     summarise(
-      pH.Led.avg = mean(pH.LED),
+      pH.Led.avg = mean(pH.LED,na.rm=T),
       Gain.avg = mean(Gain, na.rm = T),
-      O2.Led.avg = mean(O2.LED),
+      O2.Led.avg = mean(O2.LED,na.rm=T),
       KSV.avg = mean(KSV, na.rm = T)
-    ) %>% select(-sn)
+    ) %>% select(-sn) %>%
+  as.data.frame(lapply(.,nan2na))
 }
