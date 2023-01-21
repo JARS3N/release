6c0e49bdb6fed71f0dd1bf3dfa2494fb66fda3fa
JARS3N <JARS3N@users.noreply.github.com>
2023-01-21 01:42:51 -0500

    Create revised_ui.R

---
+++ R/revised_ui.R
@@ -0,0 +1,25 @@
+revised_ui <- function() {
+  fluidPage(
+    titlePanel('Download a PDF report'),
+    shinyjs::useShinyjs(),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          "lotselect",
+          label = "Lot",
+          choices = "...",
+          selected = 1
+        ),
+        selectInput(
+          "selInst",
+          label = h3("QC Instrument"),
+          choices = "..."
+        ),
+        actionButton("Compiler", "compile data"),
+        shinyjs::hidden(downloadButton('downloadData', 'print PDF'))
+      ),
+      mainPanel(h3(textOutput("lotchoice")),
+                DT::dataTableOutput("sumtbl"))
+    )
+  )
+}
