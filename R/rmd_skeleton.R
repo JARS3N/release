51057192ed1774edffd045e583a4ba2f06a4abcc
JARS3N <JARS3N@users.noreply.github.com>
2023-02-17 14:49:22 -0500

    Update rmd_skeleton.R

--- R/rmd_skeleton.R
+++ R/rmd_skeleton.R
@@ -19,7 +19,9 @@ rmd_skeleton<-function(){
     rep(blank,3),
     "### Approved for Release by:  " ,
     sig_section,
-    "### Deviation Approved by:  ",
+  #  "### Deviation Approved by:  ",
+    "### Reference the following site-specific procedures when deviation from the process is applicable:",
+"#### SBS-1303-600 Procedure Deviation Process, SBS-1302-700 Form Deviation, SBS-1301-700 Request Log:",
     sig_section
   )
 }
